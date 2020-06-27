4e44cbf793277fe4cdf16d8066f187e34543a84c
Michał Wodniczak <michal.wodniczak@o2.pl>
2020-06-27 22:27:55 +0200

    refactory - add function

--- js/first.js
+++ js/first.js
@@ -1,33 +1,46 @@
-console.log("siema");
-
-let button = document.querySelector(".js-buttonSide");
-let side = document.querySelector(".container");
-let secretButton = document.querySelector(".secretRoom__buttonSecret");
-let secret = document.querySelector(".secretRoom__secretText")
-let destroy = document.querySelector(".secretRoom__destroyButton");
-let headerH2 = document.querySelector(".header__h2");
-let articleHeader = document.querySelectorAll(".article__header"); // need help here ;d
-let tableCaption = document.querySelector(".article__tableCaption");
-
-//change theme
-button.addEventListener("click", () => {
-    side.classList.toggle("dark");
-    headerH2.classList.toggle("header__h2--dark");
-    articleHeader.classList.toggle("article__header--dark");
-    tableCaption.classLista.toggle("article__tableCaption--dark");
-    if (button.innerText === "Przejdź na mroczną strone mocy") {
-        button.innerText = "Przejdź na jasną strone mocy";
-    } else {
-        button.innerText = "Przejdź na mroczną strone mocy";
+{
+    const welcome = () => {
+        console.log("siema");
     }
-});
-//add secret text
-secretButton.addEventListener("click", () => {
-    secret.innerHTML = "<h3>Dziękuje Za uwage ;). Zapraszam Na Slacka do wyrażenia opini:))</h3>";
-    
-});
+    welcome();
 
-destroy.addEventListener("click", () => {
-    side.remove();
+    const side = document.querySelector(".container");
 
-});
+    const toogleTheme = () => {
+        const button = document.querySelector(".js-buttonSide");
+        const headerH2 = document.querySelector(".header__h2");
+        const articleHeader = document.querySelectorAll(".article__header"); // need help here ;d
+        const tableCaption = document.querySelector(".article__tableCaption");
+        button.addEventListener("click", () => {
+            side.classList.toggle("dark");
+            headerH2.classList.toggle("header__h2--dark");
+            articleHeader.classList.toggle("article__header--dark");
+            tableCaption.classLista.toggle("article__tableCaption--dark");
+            if (button.innerText === "Przejdź na mroczną strone mocy") {
+                button.innerText = "Przejdź na jasną strone mocy";
+            } else {
+                button.innerText = "Przejdź na mroczną strone mocy";
+            }
+        });
+    }
+    toogleTheme();
+
+    const onSecretButtonClick = () => {
+        const secretButton = document.querySelector(".secretRoom__buttonSecret");
+        const secret = document.querySelector(".secretRoom__secretText")
+        secretButton.addEventListener("click", () => {
+            secret.innerHTML = "<h3>Dziękuje Za uwage ;). Zapraszam Na Slacka do wyrażenia opini:))</h3>";
+
+        });
+    }
+    onSecretButtonClick();
+
+    const onDestroyButtonClick = () => {
+        const destroy = document.querySelector(".secretRoom__destroyButton");
+        destroy.addEventListener("click", () => {
+            side.remove();
+
+        });
+    };
+    onDestroyButtonClick();
+}
